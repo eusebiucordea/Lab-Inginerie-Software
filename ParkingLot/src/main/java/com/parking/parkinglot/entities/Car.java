a925fd7b25fbc82720f4b86a09a72410b3fad017
Cordea Eusebiu <150179026+eusebiucordea@users.noreply.github.com>
2024-11-12 09:01:13 +0200

    created the entities

---
+++ ParkingLot/src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,51 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    private Long id;
+
+    private User owner;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    private String licensePlate;
+
+    private String parkingSpot;
+
+    @ManyToOne
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+}
